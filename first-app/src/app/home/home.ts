d29e5fb518bacd3ec9314870df2088781dd9abde
Arthur Tavares <arthur.xvtv@gmail.com>
2026-06-11 13:47:18 -0300

    Lesson 4

--- first-app/src/app/home/home.ts
+++ first-app/src/app/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HousingLocation } from '../housing-location/housing-location';
+import {HousingLocationInfo} from '../housinglocation'
+// import {} from ''
 
 @Component({
   selector: 'app-home',
@@ -8,5 +10,16 @@ import { HousingLocation } from '../housing-location/housing-location';
   styleUrls: ['./home.css'],
 })
 export class Home {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
 
+  housingLocation: HousingLocationInfo = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
